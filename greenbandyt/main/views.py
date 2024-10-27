01f7f8e09ae32f979218c417366272e993bf76d8
Yuriy Bandura <bandurayv@gmail.com>
2024-10-27 21:39:24 +0300

    Add_Eny_Code_2

--- greenbandyt/main/views.py
+++ greenbandyt/main/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("<h1> Мой проект на ДЖАНГО </h1>")
+
+def new(request):
+    return HttpResponse("<h1>Вторая страница проекта на ДЖАНГО </h1>")
