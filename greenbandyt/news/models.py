0bb1f980140376bbc3ca6de695fcd73eea859aab
Yuriy Bandura <bandurayv@gmail.com>
2024-10-30 23:48:15 +0300

    Add_Step_3

--- greenbandyt/news/models.py
+++ greenbandyt/news/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class News_post(models.Model):
+    title = models.CharField('Название новости',max_length=50)
+    short_description = models.CharField('Краткое описание',max_length=200)
+    text = models.TextField('Новость')
+    date = models.DateTimeField('Дата публикации',auto_now_add=True)
+    author = models.CharField('Автор',max_length=200)
