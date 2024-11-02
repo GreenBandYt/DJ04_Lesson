13078a59ebe3665fd40fe88e05f28e474ecd26af
Yuriy Bandura <bandurayv@gmail.com>
2024-11-02 22:29:10 +0300

    Add_5

--- greenbandyt/news/views.py
+++ greenbandyt/news/views.py
@@ -9,5 +9,13 @@ def home(request):
     return render(request, 'news/news.html', {'news': news})
 
 def news_add(request):
+    errors = ""
+    if request.method == "POST":
+        form = NewsForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, 'news/news.html')
+        else:
+            errors = "Данные заполнены неверно"
     form = NewsForm()
-    return render(request, 'news/news_add.html', {'form': form})
+    return render(request, 'news/news_add.html', {'form': form, 'errors': errors})
